48e3a8016df8310cf8e9355a77f306e624e5e153
lavikara <karatemitayo@gmail.com>
2021-01-07 16:50:08 +0100

    refactored store to use modules

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,22 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
+
+import state from "./state";
+import * as getters from "./getters";
+import * as mutations from "./mutations";
+import * as actions from "./actions";
+import accountModule from "./accountModule/index";
+import productModule from "./productModule/index";
 
 export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
+  state,
+  getters,
+  mutations,
+  actions,
   modules: {
-  }
-})
+    accountModule,
+    productModule,
+  },
+});
