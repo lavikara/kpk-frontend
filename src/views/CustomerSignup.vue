956450e07758c32baac0c1b36b4aa00ba156cfa2
lavikara <karatemitayo@gmail.com>
2021-01-07 17:45:18 +0100

    rendered shop nav component

--- src/views/CustomerSignup.vue
+++ src/views/CustomerSignup.vue
@@ -1,5 +1,17 @@
 <template>
   <div>
+    <ShopNav />
     <h1>hello from customer signup</h1>
   </div>
 </template>
+
+<script>
+import ShopNav from "@/components/ShopNav.vue";
+
+export default {
+  name: "CustomerSignup",
+  components: {
+    ShopNav,
+  },
+};
+</script>
