a5b5a3ed869a8dc0ebce397bc3531292d9ee1725
lavikara <karatemitayo@gmail.com>
2021-01-07 16:50:42 +0100

    changed the design of home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,44 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="home">
+    <div class="top-section container">
+      <div class="left">
+        <img
+          src="https://ng.jumia.is//cms/Homepage/2020/W40/BSB-Jumia-Plus-.gif"
+          alt="advert"
+        />
+        <img
+          src="https://www-konga-com-res.cloudinary.com/image/upload/w_auto,f_auto,fl_lossy,dpr_auto,q_auto/v1609798623/contentservice/drinksJara.png_S17kEGWCP.png"
+          alt="advert"
+        />
+      </div>
+      <div class="center">
+        <img
+          src="https://ng.jumia.is/cms/Homepage/2021/w01/1E72A15D-7BE8-4E9B-B8BD-E71F28D492B3.jpeg"
+          alt="advert"
+        />
+      </div>
+      <div class="right">
+        <img
+          src="https://ng.jumia.is//cms/Homepage/2020/W40/BSB-Jumia-Plus-.gif"
+          alt="advert"
+        />
+        <img
+          src="https://www-konga-com-res.cloudinary.com/image/upload/w_auto,f_auto,fl_lossy,dpr_auto,q_auto/v1609798623/contentservice/drinksJara.png_S17kEGWCP.png"
+          alt="advert"
+        />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  name: "Home",
+};
 </script>
+
+<style lang="scss" scoped>
+@import "@/assets/style/home.scss";
+</style>
