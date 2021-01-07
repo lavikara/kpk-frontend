1771c7e471e6c5ba0aa8f97a8704c6a663bc3f0f
lavikara <karatemitayo@gmail.com>
2021-01-07 17:03:31 +0100

    called login, signup, and create product endpoints

---
+++ src/utils/api.js
@@ -0,0 +1,26 @@
+import axios from "axios";
+import storage from "@/utils/storage.js";
+
+const BASE_URL = process.env.VUE_APP_BASE_URL;
+const USER_URL = `${BASE_URL}/user`;
+const PRODUCT_URL = `${BASE_URL}/product`;
+
+export default {
+  getHeader() {
+    let header = {
+      Authorization: "bearer " + storage.getVendorToken(),
+    };
+    return header;
+  },
+  login: (data) => {
+    return axios.post(`${USER_URL}/login`, data);
+  },
+  signupVendor: (data) => {
+    return axios.post(`${USER_URL}/signup-vendor`, data);
+  },
+  createProduct(data) {
+    return axios.post(`${PRODUCT_URL}/create`, data, {
+      headers: this.getHeader(),
+    });
+  },
+};
