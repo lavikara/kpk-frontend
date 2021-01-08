48299d7bc50c163d6d510f8801ddb7cff44ac73a
lavikara <karatemitayo@gmail.com>
2021-01-08 19:25:29 +0100

    added methods to shorten string and convert string first character to uppercase

--- src/main.js
+++ src/main.js
@@ -4,6 +4,16 @@ import router from "./router";
 import store from "./store";
 
 Vue.config.productionTip = false;
+Vue.filter("shortenString", (value) => {
+  if (value.length > 65) {
+    return value.slice(0, 45) + "...";
+  } else {
+    return value;
+  }
+});
+Vue.filter("setUppercase", (value) => {
+  return value[0].toUpperCase() + value.slice(1).toLowerCase();
+});
 
 new Vue({
   router,
