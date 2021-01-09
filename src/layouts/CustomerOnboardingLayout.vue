37c43d73ca131ba769b2df7e0e865caa16cb951c
lavikara <karatemitayo@gmail.com>
2021-01-09 09:02:04 +0100

    created customer and vendor onboarding layout

---
+++ src/layouts/CustomerOnboardingLayout.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <ShopNav />
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import ShopNav from "@/components/ShopNav.vue";
+
+export default {
+  components: {
+    ShopNav,
+  },
+  data() {
+    return {};
+  },
+
+  computed: {},
+  methods: {},
+};
+</script>
---
+++ src/layouts/VendorOnboardingLayout.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <VendorNav />
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import VendorNav from "@/components/VendorNav.vue";
+
+export default {
+  name: "vendorOnboardingLayout",
+  components: {
+    VendorNav,
+  },
+  data() {
+    return {};
+  },
+
+  computed: {},
+  methods: {},
+};
+</script>
