e39d3f53ca4e49819d183c06cf89750d013ef491
lavikara <karatemitayo@gmail.com>
2021-01-10 12:09:14 +0100

    added fade in animation

--- src/views/CustomerAccount.vue
+++ src/views/CustomerAccount.vue
@@ -1,5 +1,24 @@
 <template>
-  <div>
+  <div id="customer-account">
     <h1>Hello from customer account</h1>
   </div>
 </template>
+
+<style lang="scss" scoped>
+#customer-account {
+  animation-name: fadeIn;
+  animation-iteration-count: 1;
+  animation-timing-function: ease-in-out;
+  animation-duration: 0.8s;
+  animation-fill-mode: forwards;
+
+  @keyframes fadeIn {
+    0% {
+      opacity: 0;
+    }
+    100% {
+      opacity: 1;
+    }
+  }
+}
+</style>
