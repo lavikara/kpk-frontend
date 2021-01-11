86b66e11e6eb7cbdf21fc1be5f5da8c5eb6702dc
lavikara <karatemitayo@gmail.com>
2021-01-11 11:59:18 +0100

    promt vendor to pay registration fee after signing up

--- src/views/VendorDashboard.vue
+++ src/views/VendorDashboard.vue
@@ -1,16 +1,32 @@
 <template>
   <div id="vendor-dashboard">
-    <h1>hello from vendor dashboard</h1>
+    <h1 v-if="!loading && vendorDetails.vendor_status === true">
+      hello from vendor dashboard
+    </h1>
+    <div
+      class="approve-div container"
+      v-if="vendorDetails.vendor_status === false"
+    >
+      <h1>
+        Your shop has not been approved, please pay registration fee.
+      </h1>
+      <button @click="pay">
+        Pay<span class="loader" v-if="loading"></span>
+      </button>
+    </div>
   </div>
 </template>
 
 <script>
+import { mapActions, mapState } from "vuex";
 import storage from "@/utils/storage.js";
 
 export default {
   name: "VendorDashboard",
   data() {
-    return {};
+    return {
+      vendorDetails: "",
+    };
   },
   beforeRouteEnter: (to, from, next) => {
     if (!storage.getVendorDetails()) {
@@ -21,11 +37,39 @@ export default {
       next();
     }
   },
-  computed: {},
+  mounted() {
+    this.vendorDetails = storage.getVendorDetails().user;
+    if (this.vendorDetails.vendor_status === false) {
+      this.paymentDetails(this.vendorDetails);
+    }
+  },
+  computed: {
+    ...mapState({
+      loading: (state) => state.loading,
+      details: (state) => state.paymentModule.paymentDetails,
+      paymentUrl: (state) => state.paymentModule.paymentUrl.url,
+    }),
+  },
   methods: {
-    logout() {
-      localStorage.removeItem("vendor_details");
-      this.$router.push("/");
+    ...mapActions("paymentModule", [
+      "generateVendorPaymentLink",
+      "paymentDetails",
+    ]),
+    ...mapActions("notificationModule", ["showModal"]),
+    pay() {
+      this.showModal({
+        description: "You will be redirected to our payment platform.",
+        display: true,
+        type: "info",
+      });
+      this.generateVendorPaymentLink(this.details);
+    },
+  },
+  watch: {
+    paymentUrl(newValue, oldValue) {
+      if (newValue !== oldValue) {
+        window.location = newValue;
+      }
     },
   },
 };
@@ -45,6 +89,29 @@ export default {
     }
     100% {
       opacity: 1;
+    }
+  }
+
+  .approve-div {
+    h1 {
+      font-size: 18px;
+      margin-bottom: 1.2rem;
+    }
+  }
+  button {
+    position: relative;
+    display: flex;
+    align-items: center;
+    margin: auto;
+    background: var(--cyanBlue);
+    border-radius: 5px;
+    padding: 0.5rem 2rem;
+    margin-bottom: 1rem;
+    transition: all 0.3s;
+
+    span {
+      position: absolute;
+      right: 0.7rem;
     }
   }
 }
