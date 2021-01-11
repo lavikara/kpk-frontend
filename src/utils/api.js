1ba1a9a121b9f048527e6be05e2451d9330ece72
lavikara <karatemitayo@gmail.com>
2021-01-11 11:52:52 +0100

    added payment api endpoint

--- src/utils/api.js
+++ src/utils/api.js
@@ -5,6 +5,7 @@ const BASE_URL = process.env.VUE_APP_BASE_URL;
 const USER_URL = `${BASE_URL}/user`;
 const PRODUCT_URL = `${BASE_URL}/product`;
 const CART_URL = `${BASE_URL}/cart`;
+const PAYMENT_URL = `${BASE_URL}/payment`;
 
 export default {
   getVendorHeader() {
@@ -57,6 +58,11 @@ export default {
   deleteFromCart(data) {
     return axios.post(`${CART_URL}/delete-from-cart`, data, {
       headers: this.getCustomerToken(),
+    });
+  },
+  generateVendorPaymentLink(data) {
+    return axios.post(`${PAYMENT_URL}/generate`, data, {
+      headers: this.getVendorHeader(),
     });
   },
 };
