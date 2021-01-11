399847af364d492aa8e6d7635f965401df7e0028
lavikara <karatemitayo@gmail.com>
2021-01-11 19:01:07 +0100

    stored rider details in local storage

--- src/utils/storage.js
+++ src/utils/storage.js
@@ -5,16 +5,25 @@ export default {
   setCustomer(data) {
     return localStorage.setItem("customer_details", JSON.stringify(data));
   },
+  setRider(data) {
+    return localStorage.setItem("rider_details", JSON.stringify(data));
+  },
   getVendorDetails() {
     return JSON.parse(localStorage.getItem("vendor_details"));
   },
   getCustomerDetails() {
     return JSON.parse(localStorage.getItem("customer_details"));
   },
+  getRiderDetails() {
+    return JSON.parse(localStorage.getItem("rider_details"));
+  },
   getVendorToken() {
     return JSON.parse(localStorage.getItem("vendor_details")).token;
   },
   getCustomerToken() {
     return JSON.parse(localStorage.getItem("customer_details")).token;
+  },
+  getRiderToken() {
+    return JSON.parse(localStorage.getItem("rider_details")).token;
   },
 };
