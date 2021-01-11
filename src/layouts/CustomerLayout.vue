90a786b71227af421fd1d6d7ec7411cc41764282
lavikara <karatemitayo@gmail.com>
2021-01-11 18:55:52 +0100

    created customer layout

---
+++ src/layouts/CustomerLayout.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <ShopNav />
+    <router-view></router-view>
+  </div>
+</template>
+<script>
+import ShopNav from "@/components/ShopNav.vue";
+
+export default {
+  name: "CustomerLayout",
+  components: {
+    ShopNav,
+  },
+};
+</script>
